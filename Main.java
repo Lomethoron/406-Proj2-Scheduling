6c3318e32d9d84d2f837d6b452fa56e6bba2c021
lomethoron <wdixon23192@gmail.com>
2016-03-26 16:31:14 -0400

    Initial work: parses arguements.

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main{
+	
+	public static void main (String[] args) {
+		
+		// get stuff from arguements
+		try{
+			String sortType = args[0];
+		}
+		catch(Exception e){
+			System.out.println("Sorting method not specified, please properly format your arguement.");
+			return;
+		}
+		try{
+			String inputFile = args[1];
+		}
+		catch(Exception e){
+			System.out.println("File name not specified, please properly format your arguement.");
+			return;
+		}
+		
+		//read from file
+		
+		//switch to proper scheduling method
+		
+	}
+}
