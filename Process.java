e0f2b44538a8793543b1f1ec8755caf2f88a56a5
lomethoron <wdixon23192@gmail.com>
2016-03-26 17:54:47 -0400

    Added class for Process storage

---
+++ Process.java
@@ -0,0 +1,51 @@
+public class Process{
+	
+	private int burst, arrival, priority;
+	private String pid;
+	
+	public Process(){
+		pid = "";
+		arrival = -1;
+		burst = -1;
+		priority = -1;
+	}
+	public Process(String pid, int burst, int arrival, int priority){
+		this.pid = pid;		
+		this.burst = burst;
+		this.arrival = arrival;
+		this.priority = priority;
+	}
+	
+	public String getPid() {
+		return pid;
+	}
+	
+	public int getBurst() {
+		return burst;
+	}
+	
+	public int getArrival(){
+		return arrival;
+	}
+	
+	public int getPriority(){
+		return priority;
+	}
+	
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	
+	public void setBurst(int burst) {
+		this.burst = burst;
+	}
+	
+	public void setArrival(int arrival){
+		this.arrival = arrival;
+	}
+	
+	public void getPriority(int priority){
+		this.priority = priority;
+	}
+	
+}
